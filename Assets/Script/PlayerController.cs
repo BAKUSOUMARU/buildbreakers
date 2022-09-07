31e89fb06bce0a3de662e4b6d030e85feb13cbb7
BAKUSOUMARU <bakusoumaro@gmail.com>
2022-09-07 13:13:32 +0900

    プレイヤーを動くようにした

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float _speed = 0;
 
-    Rigidbody _rd;
+    /// <summary>動く速さ</summary>
+    [SerializeField] float m_movingSpeed = 5f;
+    /// <summary>ターンの速さ</summary>
+    [SerializeField] float m_turnSpeed = 3f;
+    [SerializeField] float m_isGroundedLength = 1.1f;
+    [SerializeField] float m_jumpPower = 5f;
+
+
+    Rigidbody _rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        _rd = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -22,28 +29,42 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
-        float HorizontalKey = Input.GetAxis("Horizontal");
-        float VerticalKey = Input.GetAxis("Vertical");
-        float JumpKey = Input.GetAxis("Jump");
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
 
+        Vector3 dir = Vector3.forward * v + Vector3.right * h;
+        if (dir == Vector3.zero)
+        {
+            _rb.velocity = new Vector3(0f, _rb.velocity.y, 0f);
+        }
+        else
+        {
+            // カメラを基準に入力が上下=奥/手前, 左右=左右にキャラクターを向ける
+        
+            dir = Camera.main.transform.TransformDirection(dir);    // メインカメラを基準に入力方向のベクトルを変換する
+            
+            dir.y = 0;  // y 軸方向はゼロにして水平方向のベクトルにする
+            
+            Quaternion targetRotation = Quaternion.LookRotation(dir);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRotation, Time.deltaTime * m_turnSpeed);
+            Vector3 velo = dir.normalized * m_movingSpeed; // 入力した方向に移動する
+            velo.y = _rb.velocity.y;   // ジャンプした時の y 軸方向の速度を保持する
+            _rb.velocity = velo;   // 計算した速度ベクトルをセットする
+        }
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            _rb.AddForce(Vector3.up * m_jumpPower, ForceMode.Impulse);
 
-        if (HorizontalKey > 0)
-        {
-            _rd.velocity = new Vector3(HorizontalKey * _speed, _rd.velocity.y,_rd.velocity.z);
-        }
-        else if (HorizontalKey < 0)
-        {
-            _rd.velocity = new Vector3(HorizontalKey * _speed, _rd.velocity.y, _rd.velocity.z);
-        }
-
-        if (VerticalKey > 0)
-        {
-            _rd.velocity = new Vector3(_rd.velocity.x, _rd.velocity.y, VerticalKey * _speed);
-        }
-        else if(VerticalKey < 0)
-        {
-            _rd.velocity = new Vector3(_rd.velocity.x, _rd.velocity.y, VerticalKey * _speed);
         }
     }
-        
+    bool IsGrounded()
+    {
+        // Physics.Linecast() を使って足元から線を張り、そこに何かが衝突していたら true とする
+        CapsuleCollider col = GetComponent<CapsuleCollider>();
+        Vector3 start = this.transform.position + col.center;   // start: 体の中心
+        Vector3 end = start + Vector3.down * m_isGroundedLength;  // end: start から真下の地点
+        Debug.DrawLine(start, end); // 動作確認用に Scene ウィンドウ上で線を表示する
+        bool isGrounded = Physics.Linecast(start, end); // 引いたラインに何かがぶつかっていたら true とする
+        return isGrounded;
+    }
 }
